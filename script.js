0afbb02cd62034f6a2a0bcb69c63d790cfef5097
AnamFatimaN <150925698+AnamFatimaN@users.noreply.github.com>
2024-01-08 10:56:46 +0500

    added form validation
    added form validation

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 console.log("anam")
+let id=(id)=>document.getElementById(id);
+let classes=(classes)=>document.getElementsByClassName(classes);
+let username1=id("username");
+let email=id("email");
+let error=classes("error");
+let button1=id("button1");
+button1.addEventListener("click",(e)=>{
+    e.preventDefault();
+    engine(username1, 0, "Username cannot be blank");
+    engine(email, 1, "Email cannot be blank");
+    e.reset()
+})
+
+let engine=(id,serial,message)=>{
+    if (id.value.trim() === "") {
+        error[serial].innerHTML = message;
+      } 
+      
+      else {
+        showusername()
+      }
+    }
+
 function showusername(){
-    
-    var username = document.getElementById("username").value;
+     var username = document.getElementById("username").value;
     localStorage.setItem("username",username);
     showPopUp(username);
     }
